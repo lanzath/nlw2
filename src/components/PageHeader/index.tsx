a6d6f3fefc94bede420be156085ab9a9c7d1abc9
Lanza <lanzathm@gmail.com>
2020-08-03 20:16:23 -0300

    :sparkles: Header component

---
+++ src/components/PageHeader/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logoImg from '../../assets/images/logo.svg';
+import backIcon from '../../assets/images/icons/back.svg';
+import './styles.css';
+
+// Typescript interface, required string type title
+interface PageHeaderProps {
+  title: string;
+}
+
+/**
+ * Header component
+ * @param props use props.title to get element title property
+ * @returns React function component
+ */
+const PageHeader: React.FC<PageHeaderProps> = (props) => {
+  return (
+    <header className="page-header">
+      <div className="top-bar-container">
+        <Link to="/">
+          <img src={backIcon} alt="voltar" />
+        </Link>
+        <img src={logoImg} alt="Proffy" />
+      </div>
+
+      <div className="header-content">
+        <strong>{props.title}</strong>
+
+        {props.children}
+      </div>
+
+    </header>
+  );
+}
+
+export default PageHeader;
