b4901e01861c27cc4fe505520ca6105c25f41401
Lanza <lanzathm@gmail.com>
2020-08-03 18:23:52 -0300

    :tada: Teacher list component created

---
+++ src/pages/TeacherList/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css';
+
+/**
+ * Teacher List component
+ * @returns {Element} TSX
+ */
+function TeacherList() {
+  return (
+    <h1>TeacherList</h1>
+  );
+}
+
+export default TeacherList;
