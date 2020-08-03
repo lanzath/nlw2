9ecb190742b68f57fe849edf8566cdf0403ea242
Lanza <lanzathm@gmail.com>
2020-08-03 18:23:22 -0300

    :tada: New teacher form component created

---
+++ src/pages/TeacherForm/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css';
+
+/**
+ * New Teacher Form component
+ * @returns {Element} TSX
+ */
+function TeacherForm() {
+  return (
+    <h1>TeacherForm</h1>
+  );
+}
+
+export default TeacherForm;
