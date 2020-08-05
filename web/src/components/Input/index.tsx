a18d80f0a6e9cbf119bf9fd8127e3fe987141219
Lanza <lanzathm@gmail.com>
2020-08-05 17:51:09 -0300

    :sparkles:  Input component

---
+++ web/src/components/Input/index.tsx
@@ -0,0 +1,26 @@
+import React, { InputHTMLAttributes } from 'react';
+
+import './styles.css';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  label: string;
+}
+
+/**
+ * Input component
+ * @params label
+ * @param name
+ * @param ...rest from InputHTMLAttributes
+ * @returns React component
+ */
+const Input: React.FC<InputProps> = ({ label, name, ...rest }) => {
+  return (
+    <div className="input-block">
+      <label htmlFor={name}>{label}</label>
+      <input type="text" id={name} {...rest} />
+    </div>
+  );
+}
+
+export default Input;
