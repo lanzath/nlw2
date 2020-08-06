e316f7aa9f8150074bf06d33e2cab8a591ab6de2
Lanza <lanzathm@gmail.com>
2020-08-06 19:55:30 -0300

    :sparkles: Navigation working

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,32 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { AppLoading } from 'expo';
+import AppStack from './src/routes/AppStack';
+import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo';
+import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  let [fontsLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_700Bold,
+    Poppins_400Regular,
+    Poppins_600SemiBold
+  });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#333',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  title: {
-      fontSize: 28,
-      color: '#fff',
-      fontWeight: 'bold',
-      transform: [
-          {
-              rotateZ: '25deg'
-          }
-      ]
+  if (!fontsLoaded) {
+    return <AppLoading />
+  } else {
+    return (
+      <>
+        <AppStack />
+        <StatusBar style="light" />
+      </>
+    );
   }
-});
+}
